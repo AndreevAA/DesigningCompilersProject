90b11f67ac8ac2480a88f52421ff84f1cb4bee82
AndreevAA <58621780+AndreevAA@users.noreply.github.com>
2024-05-27 08:00:00 +0300

    Updated start: without menu

--- src/__main__.py
+++ src/__main__.py
@@ -6,10 +6,31 @@
 
 # import isa
 
+# import OssCompiler
+
+import system
+import processor
+import compiler
+
+# tmp_compiler = Compiler()
+# # tmp_compiler.compile()
+# tmp_compiler.decode()
+# tmp_compiler.load()
+# tmp_compiler.execute("Multiply 5 5")
+# # tmp_compiler.state()
+
 from constants.isa import *
 
 def main():
-	print(ADD)
+    # Test compiler
+    compiler.Compile()
+    compiler.Decode()
+    compiler.Load()
+    # processor.State()
+
+    # Execute the code
+    compiler.Exec("Multiply 5 5")
+
 
 if __name__ == '__main__':
     main()
