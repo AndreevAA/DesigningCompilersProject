2505bb343ce895ac7a64ae3267221293dcc92d76
Andreev Alexander <andreev.business.mail@gmail.com>
2024-06-01 14:10:03 +0300

    update

--- src/output_executable/program.asm
+++ src/output_executable/program.asm
@@ -1,75 +1,55 @@
 .section .data
-input_format: .string "%d"
-output_format: .string "%d "
+input_format: .string "%ld"
+output_format: .string "%ld "
 
 .section .bss
-.space 400          
+    .lcomm array, 400          
 
 .section .text
 .globl _start
 
-read_int:
-    mov $0, %rax        
-    mov $0, %rdi        
-    lea 8(%rsp), %rsi    
-    mov $4, %rdx        
-    syscall
-    mov $0, %rax        
-    lea 8(%rsp), %rsi    
-    lea 16(%rsp), %rdi    
-    call atoi
-    mov 16(%rsp), %rax    
-    ret
-
-write_int:
-    lea 12(%rsp), %rsi 
-    lea output_format(%rip), %rdi 
-    mov $0, %rax
-    call printf
-    ret
-
 _start:
-    call read_int
-    mov %rax, %rcx    
+    mov $0, %rax              
+    mov $0, %rdi              
+    lea array(%rip), %rsi     
+    mov $400, %rdx            
+    syscall                   
 
-    mov %rsp, %rbx
-    sub $400, %rsp    
-
-input_loop:
-    cmp %rax, %rcx
-    je reverse_array    
-    call read_int
-    mov %rax, (%rsp,%rax,4) 
-    inc %rax
-    jmp input_loop
-
-reverse_array:
-    xor %rax, %rax 
-    lea -4(%rsp,%rcx,4), %rbx 
-reverse_loop:
-    cmp %rax, %rbx
-    jge print_array 
-    mov (%rsp,%rax,4), %rdx 
-    mov (%rbx), %rdi
-    mov %rdi, (%rsp,%rax,4)
-    mov %rdx, (%rbx)
-    add $4, %rax
-    sub $4, %rbx
-    jmp reverse_loop
-
-print_array:
-    xor %rax, %rax  
-    mov %rcx, %rdi 
-
-output_loop:
-    cmp %rax, %rdi
-    je end_program  
-    mov (%rbx,%rax,4), %rsi 
-    call write_int
-    inc %rax
-    jmp output_loop
-
-end_program:
-    mov $60, %rax  
-    xor %edi, %edi
+    mov $0, %rcx              
+    mov $100, %r8             
+.read_loop:
+    cmp %rcx, %r8             
+    jge .reverse_loop_start   
+    lea array(%rip), %rax     
+    add %rcx, %rax            
+    mov (%rax), %rdi          
+    
+    mov $1, %rax              
+    mov $1, %rdi              
+    lea output_format(%rip), %rsi
+    mov $2, %rdx              
     syscall
+    
+    inc %rcx                  
+    jmp .read_loop            
+
+.reverse_loop_start:
+    mov $0, %rax              
+.reverse_loop:
+    cmp %rax, %r8
+    jge .end_program          
+    lea array(%rip), %rbx     
+    lea array(%rip), %rcx     
+    sub %rax, %r8             
+    add %r8, %rcx             
+    mov (%rbx), %rdi
+    mov (%rcx), %rsi
+    mov %rsi, (%rbx)
+    mov %rdi, (%rcx)
+    inc %rax
+    jmp .reverse_loop
+    
+.end_program:
+    mov $60, %rax         
+    xor %edi, %edi        
+    syscall               
