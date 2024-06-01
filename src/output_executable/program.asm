e03405f7f08b458f6d4ed431be0537496eb65d27
Andreev Alexander <andreev.business.mail@gmail.com>
2024-06-01 13:51:10 +0300

    u[date

--- src/output_executable/program.asm
+++ src/output_executable/program.asm
@@ -3,25 +3,25 @@ input_format: .string "%d"
 output_format: .string "%d "
 
 .section .bss
-n:  .skip 4
-a:  .skip 40
-b:  .skip 40  
+n:  .space 4
+a:  .space 40
+b:  .space 40  
 
 .section .text
 .global _start
 
 _start:
     mov rdi, n       
-    mov rsi, input_format  
+    lea rsi, [input_format]
     call scanf
 
     xor ebx, ebx        
 
 read_loop:
-    cmp ebx, dword [n]  
+    cmp ebx, dword ptr [n]
     jge reverse_array  
-    lea rdi, [a + ebx * 4] 
-    mov rsi, input_format  
+    lea rdi, [a + ebx * 4]
+    lea rsi, [input_format]
     call scanf
     inc ebx
     jmp read_loop
@@ -30,13 +30,13 @@ reverse_array:
     xor ebx, ebx        
 
 reverse_loop:
-    cmp ebx, dword [n]
+    cmp ebx, dword ptr [n]
     jge output_array  
-    mov eax, dword [a + ebx * 4]
-    mov edx, dword [n]
+    mov eax, dword ptr [a + ebx * 4]
+    mov edx, dword ptr [n]
     sub edx, ebx
     dec edx
-    mov [b + edx * 4], eax
+    mov dword ptr [b + edx * 4], eax
     inc ebx
     jmp reverse_loop
 
@@ -44,11 +44,11 @@ output_array:
     xor ebx, ebx        
 
 output_loop:
-    cmp ebx, dword [n]
+    cmp ebx, dword ptr [n]
     jge end_program   
-    mov eax, dword [b + ebx * 4]
+    mov eax, dword ptr [b + ebx * 4]
     mov rsi, eax
-    mov rdi, output_format
+    lea rdi, [output_format]
     call printf
     inc ebx
     jmp output_loop
